5da2bdc1947391705ee4e28138fb36b038691dee
JWP2023 <garook@e-mirim.hs.kr>
2023-05-24 09:57:18 +0900

    express 실행해보기

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-const http = require('http');
-// const fs = require('fs');
-const url = require('url');
+const express = require('express');
 
-const app = http.createServer(function(req, res) {
-  let _url = req.url;   // 문자열 형태
-  // 문자열 형태의 _url을 object형태로 변환
-  let queryData = url.parse(_url, true).query;
-  res.end(queryData.id);
+const app = express();
+
+app.get('/', function(req, res, next) {
+  res.send('Hello World');
 });
 
 app.listen(3333);
