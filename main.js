a6f89799062ce337d6bf28c5866edf8bf6ba1e3a
JWP2023 <garook@e-mirim.hs.kr>
2023-05-31 09:13:09 +0900

    nodemon 설치 및 익스프레스 '*'

--- main.js
+++ main.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
+// 모든 경로에 요청
+app.get('*', function(req, res, next) {
+  console.log('당장');
+  next();
+})
+
+// 루트(/) 경로에 요청
 app.get('/', function(req, res, next) {
-  res.send('Hello World');
+  console.log('진행시켜');
+  next();
 });
 
 app.listen(3333);
